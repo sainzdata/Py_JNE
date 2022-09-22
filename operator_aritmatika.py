739d3d4eb0359837eac7d7134f803e1628196da8
saizdata <saizdata@outlook.com>
2022-09-22 15:57:23 +0700

    c

---
+++ operator_aritmatika.py
@@ -0,0 +1,27 @@
+# Ambil input untuk mengisi nilai
+a = input("Inputkan nilai a: ")
+b = input("Inputkan nilai b: ")
+
+# Menggunakan operator penjumlahan
+c = a + b
+print "Hasil %d + %d = %d" % (a,b,c)
+
+# Operator Pengurangan
+c = a - b
+print "Hasil %d - %d = %d" % (a,b,c)
+
+# Operator Perkalian
+c = a * b
+print "Hasil %d * %d = %d" % (a,b,c)
+
+# Operator Pembagian
+c = a / b
+print "Hasil %d / %d = %d" % (a,b,c)
+
+# Operator Sisa Bagi
+c = a % b
+print "Hasil %d %% %d = %d" % (a,b,c)
+
+# Operator Pangkat
+c = a ** b
+print "Hasil %d ** %d = %d" % (a,b,c)
